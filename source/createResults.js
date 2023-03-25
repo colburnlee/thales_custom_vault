c16c2938d2585b6e12277b61edded8a97e7ebc89
Lee Colburn <colburn.lee@gmail.com>
2023-03-25 08:00:30 -1000

    adding roi tracking

--- source/createResults.js
+++ source/createResults.js
@@ -7,9 +7,7 @@ const ThalesAMM = require("../contracts/ThalesAMM.js");
 // const PositionalMarketDataContract = require("../contracts/PositionalMarketData.js");
 const marketschecker = require("./marketschecker.js");
 const fs = require("fs");
-const data = require("../data.json");
-
-const testData = require("../data/archive/round_17.json");
+const allResults = require("../data/results.json");
 
 // Takes in the round data json. Function will loop throught he tradeLog and add a result field to each market. Result will be '0' for a loss, '1' for a win.
 
@@ -97,41 +95,48 @@ const getNetworkVariables = async (network) => {
 const createSummary = async (roundData) => {
   let summary = {
     round: 0,
-    Markets: roundData.tradeLog.length,
     trades: 0,
     wins: 0,
     losses: 0,
     amountWon: 0,
     amountLost: 0,
     invested: 0,
+    profit: 0,
     winPercent: 0,
-    LossPercent: 0,
+    lossPercent: 0,
   };
 
   for (let i = 0; i < roundData.tradeLog.length; i++) {
     if (i === 0) {
-      summary.round = roundData.tradeLog[i].round;
+      summary.round = +roundData.tradeLog[i].round;
     }
     let market = roundData.tradeLog[i];
     summary.trades += 1;
-    summary.invested += +market.quote.toFixed(2);
+    summary.invested += +market.quote;
     if (
       (market.result === "UP" && market.position === "UP") ||
       (market.result === "DOWN" && market.position === "DOWN")
     ) {
       summary.wins += 1;
-      summary.amountWon += +market.amount - +market.quote.toFixed(2);
+      summary.amountWon += +market.amount - +market.quote;
     } else if (
       (market.result === "UP" && market.position === "DOWN") ||
       (market.result === "DOWN" && market.position === "UP")
     ) {
       summary.losses += 1;
-      summary.amountLost += +market.quote.toFixed(2);
+      summary.amountLost += +market.quote;
     }
   }
 
-  summary.winPercent = ((summary.wins / summary.trades) * 100).toFixed(0);
-  summary.LossPercent = ((summary.losses / summary.trades) * 100).toFixed(0);
+  summary.winPercent = (summary.wins / summary.trades) * 100;
+  summary.lossPercent = (summary.losses / summary.trades) * 100;
+
+  summary.amountWon = Number(summary.amountWon.toFixed(2));
+  summary.amountLost = Number(summary.amountLost.toFixed(2));
+  summary.invested = Number(summary.invested.toFixed(2));
+  summary.winPercent = Number(summary.winPercent.toFixed(2));
+  summary.lossPercent = Number(summary.lossPercent.toFixed(2));
+  summary.profit = Number((summary.amountWon - summary.amountLost).toFixed(2));
 
   console.log(summary);
 
@@ -141,9 +146,11 @@ const createSummary = async (roundData) => {
 module.exports = createResults;
 
 const main = async () => {
-  let roundData = testData;
+  const roundData = require("../data/archive/round_18.json"); // Change this to the round you want to process
   let roundDataWithResults = await createResults(roundData);
   let summary = await createSummary(roundDataWithResults);
+  allResults.roundResults.push(summary);
+  fs.writeFileSync("./data/results.json", JSON.stringify(allResults, null, 2));
 };
 
 main();
